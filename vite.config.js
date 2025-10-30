ebde1c5f34d87dfba11d2204b875fe3c4da8e3e3
choubeyjeet <choubeyjeet2580@gmail.com>
2025-10-30 15:13:03 +0530

    Updated Code with fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // important when deploying on static hosts
+  build: {
+    outDir: 'dist'
+  }
 })
